2610d5fa97d12b005ef1ed9beca9025bbcf6d80f
Alex Beaver <alex@abvr.xyz>
2020-02-18 13:29:14 -0800

    Base Project Additions

--- PIDTuning/src/main/java/frc/robot/subsystems/ExampleSubsystem.java
+++ PIDTuning/src/main/java/frc/robot/subsystems/ExampleSubsystem.java
@@ -7,21 +7,81 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ExampleSubsystem extends SubsystemBase {
 
-  
+  public TalonFX leftMaster;
+  public TalonFX leftFollower;
+  public TalonFX rightMaster;
+  public TalonFX rightFollower;
+
+  /**
+   * Puts default values to SmartDashboard
+   */
+  public void putToSmartDashboard() {
+    SmartDashboard.putBoolean("Flush", false);
+    SmartDashboard.putNumber("kP", 0);
+    SmartDashboard.putNumber("kI", 0);
+    SmartDashboard.putNumber("kD", 0);
+    SmartDashboard.putNumber("kF", 0);
+    SmartDashboard.putNumber("Setpoint", 0);
+  }
 
   /**
    * Creates a new ExampleSubsystem.
    */
   public ExampleSubsystem() {
+    leftMaster = new TalonFX(0);
+    leftFollower = new TalonFX(1);
+    rightMaster = new TalonFX(15);
+    rightFollower = new TalonFX(14);
 
+    if (SmartDashboard.getBoolean("published", false) == false) {
+      putToSmartDashboard();
+    }
+    flush();
+
+  }
+
+  /**
+   * Flushes the latest SmartDashboard values to the motors
+   */
+  public void flush() {
+    TalonFX[] motors = { leftMaster, leftFollower, rightMaster, rightFollower };
+    for (TalonFX motor : motors) {
+      motor.config_kP(0, SmartDashboard.getNumber("kP", 0));
+      motor.config_kI(0, SmartDashboard.getNumber("kI", 0));
+      motor.config_kD(0, SmartDashboard.getNumber("kD", 0));
+      motor.config_kF(0, SmartDashboard.getNumber("kF", 0));
+      motor.set(ControlMode.Velocity, SmartDashboard.getNumber("Setpoint", 0));
+
+    }
+    SmartDashboard.putBoolean("Flush", false);
+  }
+
+  /**
+   * Puts the feedback values to Smart Dashboard
+   */
+  public void putMotorValuesToSmartDashboard() {
+    TalonFX[] motors = { leftMaster, leftFollower, rightMaster, rightFollower };
+    for (TalonFX motor : motors) {
+      String baseName = "Motor " + motor.getDeviceID() + " ";
+      SmartDashboard.putNumber(baseName + "target", motor.getClosedLoopTarget());
+      SmartDashboard.putNumber(baseName + "velocity", motor.getSelectedSensorVelocity());
+
+    }
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    if (SmartDashboard.getBoolean("Flush", false)) {
+      flush();
+    }
   }
 }
